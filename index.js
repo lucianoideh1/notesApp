07a739176b5fd56efd20012c09b84914781392dc
Luciano Vazquez <vazquezluciano5@gmail.com>
2023-07-27 19:13:42 -0300

    made so there are not hardcoded tasks from html but rather from an array

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-// grab both input & btn
-// grab the article and append p's to it
 const inputField = document.getElementById('input-field')
 const addBtn = document.getElementById('add-btn')
 const tasksContainer = document.getElementById('tasks-container')
 
+let tasks = ["Go out","Buy groceries","Workout","Create an app"]
+let tasksEl = document.createElement('article')
+
+for(let i = 0 ;i < tasks.length;i++){
+tasksEl.innerHTML += `<p>${tasks[i]}</p>`
+}
+tasksContainer.append(tasksEl)
+ 
 addBtn.addEventListener("click",function(){
+    tasks.push(inputField.value)
+    localStorage.setItem("tasks",JSON.stringify(tasks))
     tasksContainer.innerHTML += `<p>${inputField.value}</p>`
     inputField.value = ""
 })
+
