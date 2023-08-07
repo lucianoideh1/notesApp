ca71846f59b88f4040df7510cffb42ff7ec8ddb9
Luciano Vazquez <vazquezluciano5@gmail.com>
2023-08-07 18:07:39 -0300

    now I can render depening the arr, but one still overwrites the other

--- index.js
+++ index.js
@@ -5,19 +5,28 @@ const tasksFromLs = JSON.parse(localStorage.getItem("tasks"))
 
 let tasksArr = []
 
+
+render(tasksFromLs)
 addBtn.addEventListener("click",function(){
+    //step 1 : set value in a variable, and push it into the arr
     let inputFieldValue = inputField.value
     tasksArr.push(inputFieldValue)
+    console.log(tasksArr)
+    //step 2: push the arr values into localStorage
     localStorage.setItem("tasks",JSON.stringify(tasksArr))
 
-    console.log(tasksArr)
-
-    tasksContainer.innerHTML = ""
-    for(let i=0; i < tasksArr.length; i++){
-        let currentTask = tasksArr[i]
-        tasksContainer.innerHTML += `<li>${currentTask}</li>`
-    }
-
+    //step 3.0 clear the html from container + render all of them but FROM arr
+    tasksContainer.innerHTML = ""    
+    //step 3:render back again from arr or ls
+    render(tasksArr)
+    //step 4: clear inputField
     inputField.value = ""
 })
 
+
+function render(tasks){
+    for(let i=0;i<tasks.length;i++){
+        let currentTask = tasks[i]
+        tasksContainer.innerHTML += `<li>${currentTask}</li>`
+    }
+}
