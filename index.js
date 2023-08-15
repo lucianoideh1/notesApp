0418b6773b67b42a192b1c80899897273ca30232
Luciano Vazquez <vazquezluciano5@gmail.com>
2023-08-15 14:50:16 -0300

    fixed minor things + made it work checking if ls was empty or not and same for task arr, I was having it always empty before

--- index.js
+++ index.js
@@ -3,17 +3,24 @@ const addBtn = document.getElementById('add-btn')
 const tasksContainer = document.getElementById('tasks-container')
 const tasksFromLs = JSON.parse(localStorage.getItem("tasks"))
 
-let tasksArr = []
+let tasksArr
 
+if(tasksFromLs !== "" | tasksFromLs !== null){
+    tasksArr = tasksFromLs
+    render(tasksFromLs)
+} else {
+    tasksArr = []
+}
 
-render(tasksFromLs)
 addBtn.addEventListener("click",function(){
     //step 1 : set value in a variable, and push it into the arr
     let inputFieldValue = inputField.value
-    tasksArr.push(inputFieldValue)
-    console.log(tasksArr)
-    //step 2: push the arr values into localStorage
-    localStorage.setItem("tasks",JSON.stringify(tasksArr))
+  
+    if(inputFieldValue !== ""){
+         tasksArr.push(inputFieldValue)
+         //step 2: push the arr values into localStorage
+         localStorage.setItem("tasks",JSON.stringify(tasksArr))
+    }
 
     //step 3.0 clear the html from container + render all of them but FROM arr
     tasksContainer.innerHTML = ""    
@@ -25,7 +32,7 @@ addBtn.addEventListener("click",function(){
 
 
 function render(tasks){
-    for(let i=0;i<tasks.length;i++){
+    for(let i = 0;i < tasks.length; i++){
         let currentTask = tasks[i]
         tasksContainer.innerHTML += `<li>${currentTask}</li>`
     }
